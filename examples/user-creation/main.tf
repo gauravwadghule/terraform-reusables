d28faf56d65539d81a31fda43fb97c81f1d2edc5
Gaurav Wadghule <wadghulegaurav@gmail.com>
2021-11-24 20:08:08 +0530

    module source path fixes

--- examples/user-creation/main.tf
+++ examples/user-creation/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "user" {
-  source    = "./iam-user"
+  source    = "../../modules/iam-user"
   user_name = var.user_name
   pgp_key   = "keybase:osterman"
   groups    = []
